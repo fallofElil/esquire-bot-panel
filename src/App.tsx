796474b242e5eec196dcc493b4bc69f09a4a6a7f
Ilya Kiyanov <ikiyanov@yahoo.com>
2021-04-21 05:10:31 +0300

    adds React Admin UI kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
+import { Admin } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
 
 function App(): JSX.Element {
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <Admin dataProvider={simpleRestProvider('http//0.0.0.0:3000')} />;
 }
 
 export default App;
